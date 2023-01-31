f6809c998f1e8c606931c5f499d3f2e790908a8a
Rommelo Yu <developer@discocube.org>
2023-01-31 15:33:48 +0100

    refactored time intensive edges to consider only the edges whose edges fulfill a condition.

--- easy_dc/solve.py
+++ easy_dc/solve.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 
 from collections import deque
@@ -9,26 +11,34 @@ from utils.decs import profile, time
 from utils.io import get_G
 
 
-@profile()
-def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: GLvls) -> Solution:
+# @profile()
+def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: GLvls, ET) -> Solution:
     """
-    Solves the hamiltonian cycle problem in discocube graphs deterministically using divide and conquer (non-recursive) and in linear time (the time it takes grows to solve the problem grows linearly to the size of the input) . Uses the weaving process as inspiration and metaphor for the algorithmic design and process.
+    Solves the hamiltonian cycle problem in discocube graphs deterministically using divide and conquer (
+    non-recursive) and in linear time (the time it takes grows to solve the problem grows linearly to the size of the
+    input) . Uses the weaving process as inspiration and metaphor for the algorithmic design and process.
     1. Spin yarn: create an initial hamiltonian path from the node furthest from the origin to the origin
-    2. color yarn: each level alternates in color from natural to blue. The blue yarn is a 180 rotation around the z-axis and a unit length displacement
+    2. color yarn: each level alternates in color from natural to blue. The blue yarn is a 180 rotation around the
+    z-axis and a unit length displacement
     in the y direction.
 
-    As the size of the input grows, the time it takes to solve the problem increases by a factor proportional to the input.
-    The function weave takes as input an Adjacency dictionary A, a set of vertices V and an index map VI and a weights W.
+    As the size of the input grows, the time it takes to solve the problem increases by a factor proportional to the
+    input.
+    The function weave takes as input an Adjacency dictionary A, a set of vertices V and an index map VI and a
+    weights W.
     It returns a path representing Hamiltonian cycle on a discocube graph.
-    This algorithm can also be used on diamond-shaped hexagonal prismatic honeycombs which can be accessed by the ORD = 4620
+    This algorithm can also be used on diamond-shaped hexagonal prismatic honeycombs which can be accessed by the ORD
+    = 4620
 
     Vector version (using vectors instead of nodes (indices of vectors)) doesn't make it any faster.
     AdjDict = Dict[int, Set[int]]: Adjacency list of index representation of vectors in the list V.
     Verts = Tuple[Tuple[int, int, int]]: Tuple of vectors.
     IdxMap = Dict[Any, int]: Maps vectors to their indices to avoid costly index lookups.
-    EAdj = Dict[FrozenSet[int], Set[FrozenSet[int]]]: Adjacent edges of an edge, ie., parallel to the key edge and 1 unit length distance away.
+    EAdj = Dict[FrozenSet[int], Set[FrozenSet[int]]]: Adjacent edges of an edge, ie., parallel to the key edge and 1
+    unit length distance away.
     Weights = Dict[int, Union[int, float]]: Weights for each node based on their accretion level.
-    GLvls = Dict[int, Dict[str, Any]]: The adjacency dictionary partitioned according to their x value, so that they are planes of x, y.
+    GLvls = Dict[int, Dict[str, Any]]: The adjacency dictionary partitioned according to their x value, so that they
+    are planes of x, y.
     """
     ends: Ends = 0, -1
 
@@ -49,10 +59,14 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
             edges: returns the current loop represented as a set of frozensets of edges.
             eadjs: returns edges parallel to and one unit length distance away from each edge in self.edges.
         """
-        def __init__(self, loop):
+
+        def __init__(self, loop, idx):
+            self.idx = idx
             self.loop: Path = list(loop)
+            self.joined = False
             self.looped = None
             self._eadjs = None
+            self.last = False
 
         @property
         def edges(self):
@@ -61,7 +75,19 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
 
             [0, 1, 2, 3] -> {frozenset([0, 1]), frozenset([1, 2]), frozenset([2, 3]), frozenset([3, 0])}
             Calculated all the time as the self.loop value constantly changes.
+
+            If index == 0, meaning the loop is the main loop (into which all other loops are incorporated)
             """
+            if not self.idx and not self.last:
+                return {
+                    frozenset(self.loop[i - 1:i + 1])
+                    for i in range(len(self.loop) - 1)
+                    if self.joined and
+                    1 == V[self.loop[i - 1]][0] == V[self.loop[i - 1]][1] == V[self.loop[i]][0] == V[self.loop[i]][1]
+                    or not self.joined and
+                    3 == V[self.loop[i - 1]][0] == V[self.loop[i]][0] and
+                    1 == V[self.loop[i - 1]][1] == V[self.loop[i]][1]
+                }
             return {*map(frozenset, zip(self.loop, self.loop[1:] + self.loop[:1]))}
 
         @property
@@ -72,7 +98,17 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
             Calculate only if the loop value has changed.
             """
             if self.loop != self.looped:
-                self._eadjs = {eadj for edge in self.edges for eadj in EA[edge]}
+                self._eadjs = {
+                    eadj
+                    for edge in map(frozenset, zip(self.loop, self.loop[1:] + self.loop[:1]))
+                    for eadj in EA[edge]
+                }
+                #   WITHOUT EA:
+                # self._eadjs = {
+                #     eadj
+                #     for u, p in map(frozenset, zip(self.loop, self.loop[1:] + self.loop[:1]))
+                #     for eadj in ET & {*map(frozenset, product(A[u] - {p}, A[p] - {u}))}
+                # }
                 self.looped = self.loop[:]
             return self._eadjs
 
@@ -85,6 +121,7 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
                 oedge (tuple): the edge to rotate the other loop to.
                 other (Loop): the other loop to join to this one.
             """
+            self.joined = True
             self.rotate_to_edge(*edge)
             other.rotate_to_edge(*(oedge if oedge[0] in A[edge[-1]] else oedge[::-1]))
             self.loop[len(self.loop):] = other.loop
@@ -120,8 +157,10 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
         warp = (loom := warp_loom()).pop(0)
         while loom:
             for ix in loom.keys():
-                if bridge := warp.edges & (weft := loom[ix]).eadjs:
-                    if weft_e := EA[warp_e := bridge.pop()] & weft.edges:
+                if len(loom) == 1:
+                    warp.last = True
+                if bridge := warp.edges & loom[ix].eadjs:
+                    if weft_e := EA[warp_e := bridge.pop()] & loom[ix].edges:
                         warp.join(edge=tuple(warp_e), oedge=tuple(weft_e.pop()), other=loom.pop(ix))
                         break
         return warp.loop
@@ -152,19 +191,24 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
             bobbins = wind(loom) if z != -1 else None
         for w in loom:
             w += [VI[(vector := V[node])[0], vector[1], -vector[2]] for node in reversed(w)]
-        return {idx: Loop(warp) for idx, warp in enumerate(sorted(loom))}
+        return {idx: Loop(warp, idx) for idx, warp in enumerate(sorted(loom))}
 
     def spin() -> Spool:
         """
-        Walk a hamiltonian circuit starting from the node furthest from origin to towards the node closest to origin without backtracking by using
-        the calculating the attrition factor and ordering the next steps accordingly. When calculating this factor, if the start node is either the
+        Walk a hamiltonian circuit starting from the node furthest from origin to towards the node closest to origin
+        without backtracking by using
+        the calculating the attrition factor and ordering the next steps accordingly. When calculating this factor,
+        if the start node is either the
         centermost or the outermost node, it can walk all paths without backtracking.
 
-        spin() -> yarn: walk a hamiltonian circuit starting from the node furthest from origin to towards the node closest to origin. One should find
-        the hamiltonian path from the largest level in order to produce the longest piece of yarm. If the initial tour came from the least nodes ie.,
+        spin() -> yarn: walk a hamiltonian circuit starting from the node furthest from origin to towards the node
+        closest to origin. One should find
+        the hamiltonian path from the largest level in order to produce the longest piece of yarm. If the initial
+        tour came from the least nodes ie.,
         min(vector[2]) it would result in only a tour with four nodes, which is useless in creating other tours.
 
-        Color the natural thread blue by rotating the sequence vectors 180 degrees around the z-axis and displace 1 unit length along the y-axis.
+        Color the natural thread blue by rotating the sequence vectors 180 degrees around the z-axis and displace 1
+        unit length along the y-axis.
         """
         spool = [max(ZA[-1])]
         to_spin = len(ZA[-1]) - 1
@@ -181,7 +225,8 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
         This function takes in two inputs: a list called `tour` and a set called `subset`. It returns a list of lists.
         It sorts the index of the nodes in `tour` that are in `subset` and creates multiple sublists of `tour`
         based on the indices of the `subset` nodes. Finally, it returns a new list that contains each tour in
-        `subtours` in reverse order if the first element of the tour is not in `subset`, otherwise the tour is returned as is.
+        `subtours` in reverse order if the first element of the tour is not in `subset`, otherwise the tour is
+        returned as is.
 
         Args:
         tour: List of integers representing the nodes in a tour.
@@ -194,46 +239,13 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
         tour: list of integers, len(tour) > 0
         subset: set of integers, subset ⊆ tour, len(subset) > 0
 
-        Given a list S of integers representing a tour and a subset T of S, where S = {s1, s2, ..., sn} and T = {t1, t2, ..., tm},
-        the function cut(S, T) returns a new list of lists R, where each list in R is a sublist of S and satisfies the following conditions:
+        Given a list S of integers representing a tour and a subset T of S, where S = {s1, s2, ..., sn} and T = {t1,
+        t2, ..., tm},
+        the function cut(S, T) returns a new list of lists R, where each list in R is a sublist of S and satisfies
+        the following conditions:
 
             For each r in R, r is a sublist of S and is either a subset of T or its complement set (S-T)
             For each r in R, if the first element of r is not in T, then r is in reverse order.
-
-        Mathematically, this can be represented as:
-        R = {r1, r2, ..., rk} where ri ⊆ S and (r1 U r2 U ... U rk) = S and (r1 ∩ r2 ∩ ... ∩ rk) = {} and
-        ri = {sj | (sj ∈ S) and (sj ∈ T or sj ∈ (S-T))} if sj ∈ ri and sj ∈ T , else ri = {sj | (sj ∈ S) and (sj ∈ T or sj ∈ (S-T))} in reverse order.
-
-        Complexity note:
-
-        the number of bobbins increases by 2 as the level grows:
-
-        ixs = sorted((tour.index(node) for node in subset))
-        print(ixs):
-
-            [8, 11]
-            [14, 19]
-            [20, 25, 27, 36]
-            [26, 33, 35, 48, 55, 56]
-            [32, 41, 43, 62, 71, 80]
-            [38, 49, 51, 76, 87, 100, 107, 108]
-            [44, 57, 59, 90, 103, 122, 131, 140]
-            [50, 65, 67, 104, 119, 144, 155, 168, 175, 176]
-            [56, 73, 75, 118, 135, 166, 179, 198, 207, 216]
-            [62, 81, 83, 132, 151, 188, 203, 228, 239, 252, 259, 260]
-            [68, 89, 91, 146, 167, 210, 227, 258, 271, 290, 299, 308]
-            [74, 97, 99, 160, 183, 232, 251, 288, 303, 328, 339, 352, 359, 360]
-            [80, 105, 107, 174, 199, 254, 275, 318, 335, 366, 379, 398, 407, 416]
-            [86, 113, 115, 188, 215, 276, 299, 348, 367, 404, 419, 444, 455, 468, 475, 476]
-            [92, 121, 123, 202, 231, 298, 323, 378, 399, 442, 459, 490, 503, 522, 531, 540]
-            [98, 129, 131, 216, 247, 320, 347, 408, 431, 480, 499, 536, 551, 576, 587, 600, 607, 608]
-            [104, 137, 139, 230, 263, 342, 371, 438, 463, 518, 539, 582, 599, 630, 643, 662, 671, 680]
-            [110, 145, 147, 244, 279, 364, 395, 468, 495, 556, 579, 628, 647, 684, 699, 724, 735, 748, 755, 756]
-            [116, 153, 155, 258, 295, 386, 419, 498, 527, 594, 619, 674, 695, 738, 755, 786, 799, 818, 827, 836]
-            [122, 161, 163, 272, 311, 408, 443, 528, 559, 632, 659, 720, 743, 792, 811, 848, 863, 888, 899, 912, 919, 920]
-            [128, 169, 171, 286, 327, 430, 467, 558, 591, 670, 699, 766, 791, 846, 867, 910, 927, 958, 971, 990, 999, 1008]
-            [134, 177, 179, 300, 343, 452, 491, 588, 623, 708, 739, 812, 839, 900, 923, 972, 991, 1028, 1043, 1068, 1079, 1092, 1099, 1100]
-            [140, 185, 187, 314, 359, 474, 515, 618, 655, 746, 779, 858, 887, 954, 979, 1034, 1055, 1098, 1115, 1146, 1159, 1178, 1187, 1196]
         """
         subtours, prev, last_ix = [], -1, len(tour) - 1
         for e, ix in enumerate(ixs := sorted((tour.index(node) for node in subset))):
@@ -246,7 +258,8 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
 
     def wind(loom: Loom) -> NodeSet:
         """
-        returns a set of bobbins. it sets the bobbins for the next level by adding the upper and lower neighbors of the first and last element
+        returns a set of bobbins. it sets the bobbins for the next level by adding the upper and lower neighbors of
+        the first and last element
         of each thread in the loom. The method iterates over the threads in the loom and adds the upper and lower
         neighbors of the first and last element respectively, to the bobbins set and to the ends of the thread.
         """
@@ -261,15 +274,16 @@ def weave_solution(A: AdjDict, V: Verts, VI: IdxMap, EA: EAdj, W: Weights, ZA: G
 
 
 def main():
-    uon_range = 32, 32
+    uon_range = 32, 2997280
     woven, orders, all_times = None, [], []
     woven = None
     for order in uon(*uon_range):
         ord_times = []
         G = get_G(order)
-        for _ in range(1464):
+        ET = set(map(frozenset, G['E']))
+        for _ in range(10):
             start = time.time()
-            woven = weave_solution(G['A'], G['V'], G['VI'], G['EA'], G['W'], G['ZA'])
+            woven = weave_solution(G['A'], G['V'], G['VI'], G['EA'], G['W'], G['ZA'], ET)
             dur = time.time() - start
             ord_times.append(dur)
         all_times.append(min(ord_times))
